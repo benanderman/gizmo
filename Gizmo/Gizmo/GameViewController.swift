84fef116685ac99a550e3a37055d19a670595f9b
Glen DeBiasa <glen@shopspring.com>
2017-09-01 11:40:01 -0400

    Added home button.

--- Gizmo/Gizmo/GameViewController.swift
+++ Gizmo/Gizmo/GameViewController.swift
@@ -216,6 +216,11 @@ class GameViewController: UIViewController {
     showEmployee()
   }
   
+  @IBAction func homeButtonTapped(_ sender: Any) {
+    timer.invalidate()
+    navigationController?.popViewController(animated: true)
+  }
+  
   // In a storyboard-based application, you will often want to do a little preparation before navigation
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     // Get the new view controller using segue.destinationViewController.
